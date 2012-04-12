d0d064cf1354e4f835d4dce30eb5ec2145ebaa4d
Alexandru Scvortov <scvalex@gmail.com>
2012-04-12 03:08:27 +0100

    tweak default values + add Makefile

--- android/src/org/abstractbinary/toread/ListsActivity.java
+++ android/src/org/abstractbinary/toread/ListsActivity.java
@@ -3,13 +3,11 @@ package org.abstractbinary.toread;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class ListsActivity extends Activity
-{
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+public class ListsActivity extends Activity {
+        /** Called when the activity is first created. */
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.main);
+        }
 }
